919ba9ff1ab4dc0731c68cd1aa570884e981f18b
peterb <prbayne@gmail.com>
2021-01-13 13:49:58 +1300

    Get data from API

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,4 +1,38 @@
-var loggedIn = function(){
+LTB_API_KEY="asdf"
+
+function GetData(token){
+  return function(path, handler){
+    $.ajax({
+      url: `http://localhost:7071/api/${path}`,
+      headers: { 
+        "Authorization": `Bearer ${token}`,
+        "X-API-KEY": LTB_API_KEY
+      },
+      method: "GET",
+      data: {
+        year: 2021,
+        limit: 5
+      }      
+    }).done(handler)
+  }
+}
+
+function GetAllData(token){
+  const api = GetData(token)
+  api("admin/topPages", data =>{
+      $("#top3").html("")
+      data.forEach(line => {
+        $("#top3").append($(`<li>${line.src} (${line.count} students)</li>`))
+      })
+    }
+  )
+}
+
+var loggedIn = function(token){
   if(googleUser === null)
     $('.pageContent').html("Reload once signed in to view")
+  else{
+    GetAllData(token)
+  }
 }
+
--- index.md
+++ index.md
@@ -5,9 +5,8 @@ layout: dashboard
 ---
 
 ## Top 3 Pages
-1. Plan - Permits (157 students)
-1. Prepare - Notify (120 students)
-1. Plan - Smoke Plan (33 students)
+1. (Loading...)
+{: id="top3"}
 
 {% for chart in site.data.charts.dashboard %}
 {% include chart.html param=chart %}
